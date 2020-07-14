f5608507fe92b671c093845b6d5b786f574cfeb3
Vilmos Feher <jupposessho@gmail.com>
2020-07-15 00:59:29 +0200

    Pass filename as an argument

--- ziv-stream/src/main/scala/com/ziverge/Main.scala
+++ ziv-stream/src/main/scala/com/ziverge/Main.scala
@@ -9,37 +9,44 @@ import org.http4s.HttpApp
 
 import scala.collection.immutable.HashMap
 import zio._
+import zio.blocking.Blocking
+import zio.console._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 import zio.stream.ZStream
-import zio.blocking.Blocking
 
 object Main extends App {
 
   def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
 
-     val program = for {
-       ref <- Ref.make(HashMap.empty[String, HashMap[String, Int]])
-       countState = CountState(ref)
-       service = CountService(CountRepository(countState))
-       routes = CountRoutes(service).routes()
-       _ <- WordsStream.stream("/Users/feherv/Downloads/ziv.txt", service, Blocking.Service.live).fork
-       _ <- server(routes)
-       _ <- ZIO.never
-     } yield ()
+    def program(file: String) =
+      for {
+        ref <- Ref.make(HashMap.empty[String, HashMap[String, Int]])
+        countState = CountState(ref)
+        service = CountService(CountRepository(countState))
+        routes = CountRoutes(service).routes()
+        _ <- WordsStream.stream(file, service, Blocking.Service.live).fork
+        _ <- server(routes)
+        _ <- ZIO.never
+      } yield ()
 
-     program
-       .exitCode
+    args match {
+      case file :: Nil =>
+        program(file).exitCode
+      case _ =>
+        putStrLn("Missing filename argument") *> ZIO.succeed(ExitCode.failure)
+    }
   }
 
-  def server(routes: HttpApp[Task]) = ZIO.runtime[ZEnv]
-      .flatMap {
-        implicit rts =>
-          BlazeServerBuilder[Task]
-            .bindHttp(8080, "localhost")
-            .withHttpApp(routes)
-            .serve
-            .compile
-            .drain
+  def server(routes: HttpApp[Task]) =
+    ZIO
+      .runtime[ZEnv]
+      .flatMap { implicit rts =>
+        BlazeServerBuilder[Task]
+          .bindHttp(8080, "localhost")
+          .withHttpApp(routes)
+          .serve
+          .compile
+          .drain
       }
 }
